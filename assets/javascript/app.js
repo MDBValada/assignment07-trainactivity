9868c4e44e38cd18a7386e4a71cb11a35de363b3
OneOfManyMichaels <mikaerusan@gmail.com>
2019-01-29 02:00:02 -0800

    repaired errored JS and completed sceduling variable errors.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,89 +1,106 @@
 // 1. Initialize Firebase
 var config = {
-    apiKey: "AIzaSyDpIAcpds2fbIEIAh7GPx4Iy9hBSH8hGqk",
-    authDomain: "assignment07basic.firebaseapp.com",
-    databaseURL: "https://assignment07basic.firebaseio.com",
-    projectId: "assignment07basic",
-    storageBucket: "assignment07basic.appspot.com",
-    messagingSenderId: "544083161186"
-  };
+  apiKey: "AIzaSyDpIAcpds2fbIEIAh7GPx4Iy9hBSH8hGqk",
+  authDomain: "assignment07basic.firebaseapp.com",
+  databaseURL: "https://assignment07basic.firebaseio.com",
+  projectId: "assignment07basic",
+  storageBucket: "assignment07basic.appspot.com",
+  messagingSenderId: "544083161186"
+};
 
 firebase.initializeApp(config);
 
 var database = firebase.database();
+var currentTime = moment();
+//Since the -1 year seems to be the thing to do, here we go!.
+
 
 // 2. Button for adding Employees
 $("#add-train-btn").on("click", function(event) {
-  event.preventDefault();
+event.preventDefault();
 
-  // Grabs user input
-  var trainName = $("#train-name-input").val().trim();
-  var trainDestination = $("#destination-input").val().trim();
-  var trainStartTime = $("#first-train-time-input").val().trim();
-  var trainFrequency = $("#frequency-input").val().trim();
+// Grabs user input
+var trainName = $("#train-name-input").val().trim();
+var trainDestination = $("#destination-input").val().trim();
+var trainStartTime = $("#first-train-time-input").val().trim();
+var trainFrequency = $("#frequency-input").val().trim();
 
-  // Creates local "temporary" object for holding employee data
-  var newTrain = {
-    name: trainName,
-    destination: trainDestination,
-    start: trainStartTime,
-    frequency: trainFrequency
-  };
+//Math time to determine both when the next arrival time AND how many minutes away the next train currently is
+var convertedTrainStartTime = moment(trainStartTime, "hh:mm").subtract(1, "day");
+console.log(convertedTrainStartTime);
+//Establishing the time difference between the current time, and that of the first train to run.
+var timeDifference = currentTime.diff(moment(convertedTrainStartTime),"minutes");
+//need the remainder establish how much time til the next train
+var remainder = timeDifference % trainFrequency;
+var trainMinAway = trainFrequency-remainder;
+//use the trainMinAway solution to identify what time the next train is arriving
+var trainNextArrival = moment().add(trainMinAway, "minutes").format("hh:mm a");
 
-  // Uploads employee data to the database
-  database.ref().push(newTrain);
+// Creates local "temporary" object for holding employee data
+var newTrain = {
+  name: trainName,
+  destination: trainDestination,
+  start: trainStartTime,
+  frequency: trainFrequency,
+  min: trainMinAway,
+  next: trainNextArrival
+};
 
-  // Logs everything to console
-  console.log(newTrain.name);
-  console.log(newTrain.destination);
-  console.log(newTrain.start);
-  console.log(newTrain.frequency);
 
-  //alert("Train successfully added");
 
-  // Clears all of the text-boxes
-  $("#train-name-input").val("");
-  $("#destination-input").val("");
-  $("#first-train-time-input").val("");
-  $("#frequency-input").val("");
+// Uploads employee data to the database
+database.ref().push(newTrain);
+
+// Logs everything to console
+console.log(newTrain.name);
+console.log(newTrain.destination);
+console.log(newTrain.start);
+console.log(newTrain.frequency);
+console.log(newTrain.min)
+console.log(newTrain.next)
+
+
+//alert("Train successfully added");
+
+// Clears all of the text-boxes
+$("#train-name-input").val("");
+$("#destination-input").val("");
+$("#first-train-time-input").val("");
+$("#frequency-input").val("");
 });
 
 // 3. Create Firebase event for adding trains to the database and a row in the html when a user adds an entry
 database.ref().on("child_added", function(childSnapshot) {
-  console.log(childSnapshot.val());
+console.log(childSnapshot.val());
 
-  // Store everything into a variable.
-  var trainName = childSnapshot.val().name;
-  var trainDestination = childSnapshot.val().destination;
-  var trainStart = childSnapshot.val().start;
-  var trainFrequency = childSnapshot.val().frequency;
+// Store everything into a variable.
+var trainName = childSnapshot.val().name;
+var trainDestination = childSnapshot.val().destination;
+var trainStart = childSnapshot.val().start;
+var trainFrequency = childSnapshot.val().frequency;
+var minAway = childSnapshot.val().min;
+var nextArrival = childSnapshot.val().next;
 
-  // train Info
-  console.log(trainName);
-  console.log(trainDestination);
-  console.log(trainStart);
-  console.log(trainFrequency);
+// train Info
+console.log(trainName);
+console.log(trainDestination);
+console.log(trainStart);
+console.log(trainFrequency);
+console.log(minAway);
+console.log(nextArrival);
 
-  // Prettify the employee start
-  //var empStartPretty = moment.unix(empStart).format("MM/DD/YYYY");
 
-  // To calculate the months worked
-  //var empMonths = moment().diff(moment(empStart, "X"), "months");
-  //console.log(empMonths);
 
-  // Calculate the total billed rate
-  //var empBilled = empMonths * empRate;
-  //console.log(empBilled);
 
-  // Create the new row
-  var newRow = $("<tr>").append(
-    $("<td>").text(trainName),
-    $("<td>").text(trainDestination),
-    $("<td>").text(trainStart),
-    $("<td>").text(nextArrival),
-    $("<td>").text(minutesAway)
-    );
+// Create the new row
+var newRow = $("<tr>").append(
+  $("<td>").text(trainName),
+  $("<td>").text(trainDestination),
+  $("<td>").text(trainStart),
+  $("<td>").text(nextArrival),
+  $("<td>").text(minAway)
+  );
 
-  // Append the new row to the table
-  $("#schedule-table > tbody").append(newRow);
+// Append the new row to the table
+$("#schedule-table > tbody").append(newRow);
 });
---
+++ readme.md
@@ -0,0 +1,11 @@
+The 7th main assignment of the UCLA MERN Full Stack Boot Camp
+
+Using Firebase to create an application that accepts input from any user to create a fictional train list and calculate both when, and how long until the next train arrives.
+
+#Technologies used
+* Google Fonts
+* jQuery
+* Firebase
+
+Author
+Michael Bieniewicz-Valada 2018-19 - HTML/CSS/JS
